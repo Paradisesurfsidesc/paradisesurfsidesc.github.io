2098cf6ec64ec72cafd8ec2c2e0e48eb3cabe902
LEIDT <David.taylor@Leonardo.US>
2026-04-26 07:48:23 -0400

    updates

--- assets/site.js
+++ assets/site.js
@@ -12,6 +12,7 @@ document.addEventListener('DOMContentLoaded', () => {
   applyHeroImage();
   loadWeather();
   loadGA4();
+  loadKlaviyo();
 });
 
 function injectHeader() {
@@ -204,6 +205,24 @@ async function loadWeather() {
   }
 
   setTimeout(loadWeather, 5 * 60 * 1000);
+}
+
+function loadKlaviyo() {
+  const s = document.createElement('script');
+  s.src = 'https://static.klaviyo.com/onsite/js/UhvABe/klaviyo.js?company_id=UhvABe';
+  s.async = true;
+  s.type = 'text/javascript';
+  document.head.appendChild(s);
+
+  if (!window.klaviyo) {
+    window._klOnsite = window._klOnsite || [];
+    try {
+      window.klaviyo = new Proxy({}, { get: function(n, i) { return 'push' === i ? function() { var n; (n = window._klOnsite).push.apply(n, arguments); } : function() { for (var n = arguments.length, o = new Array(n), w = 0; w < n; w++) o[w] = arguments[w]; var t = 'function' == typeof o[o.length - 1] ? o.pop() : void 0, e = new Promise((function(n) { window._klOnsite.push([i].concat(o, [function(i) { t && t(i), n(i); }])); })); return e; }; } });
+    } catch(n) {
+      window.klaviyo = window.klaviyo || [];
+      window.klaviyo.push = function() { var n; (n = window._klOnsite).push.apply(n, arguments); };
+    }
+  }
 }
 
 function loadGA4() {
